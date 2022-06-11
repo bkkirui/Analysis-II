857b087c3327fa00e25a79d197ba0729af4fad88
bkkirui <63440702+bkkirui@users.noreply.github.com>
2022-06-11 09:21:20 +0300

    Update Github setup.R

--- Github setup.R
+++ Github setup.R
@@ -1,6 +1,6 @@
 library(usethis)
 ?use_github
-edit_r_environ() ##TOKEN = 'c2a1e2087e5b8dd5aeab9bddf81844f1cf8b05f2' obtained from https://github.com/settings/tokens
+edit_r_environ() ##TOKEN = 'c2a1e2087e5b8dd5aeab9bddf81844f1cf8b05f2' obtained from https://github.com/settings/tokens ##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
 library(usethis)
 use_github(protocol = "https", auth_token = Sys.getenv("GITHUB_PAT"))
 
